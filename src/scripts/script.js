6ecd0877e8a79f5231eca9e8e4ef6d9ce39098ba
Md Munna Mia <mdmunnamia987@gmail.com>
2024-01-02 21:01:16 +0600

    adding api respones using fetch

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,5 @@
 const url = "https://url-shortner10.p.rapidapi.com/lits.rocks/";
+const localStorageKey = "URLshortdata"
 const options = {
     method: "POST",
     headers: {
@@ -11,20 +12,31 @@ const options = {
     },
 };
 const longURL = document.getElementById("longURL");
+const outputURL = document.getElementById("outputBox")
 const URL_Container = [];
+
+
+async function urlShotener() {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        outputURL.innerText = result.shortUrl
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 const savaToLocalStorage = () => {
     // Some task take the input url then move with it option.body.url = input url the perfrom tasks
+    if(longURL.value === ""){
+        outputURL.innerText = "Please enter a URL!"
+        return
+    }
+    options.body.url = longURL.value;
+
+    urlShotener()
 };
-// async function find() {
-// try {
-// const response = await fetch(url, options);
-// const result = await response.json();
-// console.log(result);
-// } catch (error) {
-// console.error(error);
-// }
-// }
-//
-// find()
+
+
 const deleteFromLocalStorage = () => {};
